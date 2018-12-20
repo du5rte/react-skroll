790e1ad23b23df6c80ef564e8ae49ff533dde216
Duarte Monteiro <du5rtemonteiro@gmail.com>
2018-12-20 15:53:02 +0000

    fix tests

---
+++ demo/Demo.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Demo from './Demo';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Demo />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
