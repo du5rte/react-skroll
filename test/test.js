7c4fd5b34aa6995c8fea639a7fa57d156fb73995
Duarte Monteiro <du5rte@users.noreply.github.com>
2019-08-26 18:20:49 +0100

    add build test

---
+++ test/test.js
@@ -0,0 +1,15 @@
+import assert from 'assert'
+
+import ReactSkroll, { Scroller, ScrollLink, ScrollProvider, } from '../src'
+
+describe('Libary', () => {
+  describe('modules', () => {
+    it('should export default module', () => {
+      assert.ok(ReactSkroll)
+    })
+
+    it('should export modules', () => {
+      assert.ok(Scroller)
+    })
+  })
+})
